5c0e708ea9539209269acce7ceed4ae9dbb3b43e
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 13:05:14 +0530

    mongodb function removed

--- Database/Database.js
+++ Database/Database.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const mongoURI = 'mongodb+srv://sachinsaxenapec:6WtVUNw16x6GjZap@ticketsystem-test.wia0j2o.mongodb.net/?retryWrites=true&w=majority&appName=ticketSystem-test'
 const connectTomongo = () => {
-    mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        ssl: true,
-    }, console.log("Database is now online"))
+    mongoose.connect(mongoURI, {}, console.log("Database is now online"))
 }
 module.exports = connectTomongo;
