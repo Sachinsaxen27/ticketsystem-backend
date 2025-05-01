209681d2008927ac58212a45f2a5f941877fcaa4
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 19:09:34 +0530

    cors

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 require('dotenv').config()
 
-const express=require('express')
-const database=require('./Database/Database')
-const cors=require('cors')
+const express = require('express')
+const database = require('./Database/Database')
+const cors = require('cors')
 database()
 
-const app=express()
-const port= process.env.PORT || 5000;
-// app.use(cors(({ origin: 'http://localhost:5173', credentials: true })))
-app.use(express.json({limit:"10mb",extended:true}))
-app.use(express.urlencoded({limit:"10mb",extended:true,parameterLimit:50000}))
+const app = express()
+const port = process.env.PORT || 5000;
 
-app.use('/api/adminlogin',require('./Data/AdminData'))
-app.use('/api/memberlogin',require('./Data/MemberData'))
-app.use('/api/userlogin',require('./Data/UserData'))
-app.use('/api/messagebox',require('./Data/MessageData'))
-app.listen(port,()=>{
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://magenta-begonia-83cf73.netlify.app/']
+}));
+
+// app.options("*", cors(corsvalue))
+app.use(express.json({ limit: "10mb", extended: true }))
+app.use(express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 }))
+
+app.use('/api/adminlogin', require('./Data/AdminData'))
+app.use('/api/memberlogin', require('./Data/MemberData'))
+app.use('/api/userlogin', require('./Data/UserData'))
+app.use('/api/messagebox', require('./Data/MessageData'))
+app.listen(port, () => {
     console.log(`Ticket System is Online at http://localhost:${port}`)
 })
