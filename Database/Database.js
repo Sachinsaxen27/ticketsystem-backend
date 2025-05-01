475509231911e25c3506d7a8f1da7eabcbc5a43c
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 14:01:30 +0530

    mongodb

--- Database/Database.js
+++ Database/Database.js
@@ -5,9 +5,6 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI || 'mongodb+srv://sachinsaxenapec:6WtVUNw16x6GjZap@ticketsystem-test.wia0j2o.mongodb.net/?retryWrites=true&w=majority&appName=ticketSystem-test'
 const connectTomongo = () => {
-    mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, console.log("Database is now online"))
+    mongoose.connect(mongoURI,  console.log("Database is now online"))
 }
 module.exports = connectTomongo;
