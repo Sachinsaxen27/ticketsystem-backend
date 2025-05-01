562dfa146732c88ed534c21ad76e1baa51304ab6
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 14:06:41 +0530

    mongodb

--- Database/Database.js
+++ Database/Database.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI || 'mongodb+srv://sachinsaxenapec:6WtVUNw16x6GjZap@ticketsystem-test.wia0j2o.mongodb.net/?retryWrites=true&w=majority&appName=ticketSystem-test'
 const connectTomongo = () => {
-    mongoose.connect(mongoURI,  console.log("Database is now online"))
+    mongoose.connect(mongoURI).then(() => console.log('MongoDB connected!'))
+        .catch((err) => console.error('MongoDB connection error:', err));
 }
 module.exports = connectTomongo;
