c8c5eefb546c1bb846800700d5f0e4995947dc78
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 19:33:16 +0530

    coors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ database()
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin:'http://localhost:5173',credentials:true}));
 
 // app.options("*", cors(corsvalue))
 app.use(express.json({ limit: "10mb", extended: true }))
