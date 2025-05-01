fcce989315a973e25913b80848f215781ad9e470
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 12:53:57 +0530

    update port

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors=require('cors')
 database()
 
 const app=express()
-const port=5000
-app.use(cors(({ origin: 'http://localhost:5173', credentials: true })))
+const port= process.env.PORT || 5000;
+// app.use(cors(({ origin: 'http://localhost:5173', credentials: true })))
 app.use(express.json({limit:"10mb",extended:true}))
 app.use(express.urlencoded({limit:"10mb",extended:true,parameterLimit:50000}))
 
