5e39dba174deb09332fc772ca435f47e20bd7b5e
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-01 13:21:20 +0530

    database

--- Database/Database.js
+++ Database/Database.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
+
+
 const mongoose = require('mongoose')
-const mongoURI =process.env.MONGO_URI||'mongodb+srv://sachinsaxenapec:6WtVUNw16x6GjZap@ticketsystem-test.wia0j2o.mongodb.net/?retryWrites=true&w=majority&appName=ticketSystem-test'
+
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://sachinsaxenapec:6WtVUNw16x6GjZap@ticketsystem-test.wia0j2o.mongodb.net/?retryWrites=true&w=majority&appName=ticketSystem-test'
 const connectTomongo = () => {
-    mongoose.connect(mongoURI, {}, console.log("Database is now online"))
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, console.log("Database is now online"))
 }
 module.exports = connectTomongo;
