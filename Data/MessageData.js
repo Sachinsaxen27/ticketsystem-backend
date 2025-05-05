6f65cfbda3fe527e08e5161a1c90ffd196941c14
Sachin Saxena <saxenasachin186@gmail.com>
2025-05-05 13:43:47 +0530

    times

--- Data/MessageData.js
+++ Data/MessageData.js
@@ -188,7 +188,7 @@ router.get('/Missed_chat', async (req, res) => {
     for (let i = 0; i < conversationId.length; i++) {
         const conversation = conversationId[i];
         const adminReply = await MessageSchema.find({ conversationID: conversation._id, role: 'member' }).sort({timestamp:-1});
-        const userReply = (await MessageSchema.find({ conversationID: conversation._id, role: 'user' })).sort({timestamp:1});
+        const userReply = await MessageSchema.find({ conversationID: conversation._id, role: 'user' }).sort({timestamp:1});
 
         let userDate;
         let day, month, year;
